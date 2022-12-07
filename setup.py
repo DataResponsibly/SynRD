9db8ae22d8ac21529bf713642ec1686aaa805936
Stadnik Andrii <stadnikandriy1@gmail.com>
2022-12-07 15:17:05 +0200

    Move setup.py from setup_py branch

--- setup.py
+++ setup.py
@@ -1,1 +1,19 @@
-# TODO: Pypi package stuff HERE.
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="SynRD",
+    version="0.1",
+    description="Synthetic Datasets generator tester",
+    long_description=long_description,
+    author="Lucas Rosenblatt",
+    author_email="lr2872@nyu.edu",
+    url="https://github.com/DataResponsibly/SynRD",
+    packages=["SynRD"],
+    # setup_requires=['wheel'],
+    install_requires=["smartnoise-synth", "rpy2"],
+)
