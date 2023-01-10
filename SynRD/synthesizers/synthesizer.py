f4a0703597f389ae3ed205b0817be738357fc8b9
wonkwonlee <28593767+wonkwonlee@users.noreply.github.com>
2023-01-10 22:11:53 +0900

    Update PATECTGAN

--- SynRD/synthesizers/synthesizer.py
+++ SynRD/synthesizers/synthesizer.py
@@ -2,6 +2,11 @@ import os
 import pandas as pd
 import logging
 
+from snsynth.preprocessors import GeneralTransformer, BaseTransformer
+from snsynth.pytorch import PytorchDPSynthesizer
+from snsynth.pytorch.nn import PATECTGAN
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -64,3 +69,40 @@ class MSTSynthesizer(Synthesizer):
             mst_synth_data = self.slide_range_backward(mst_synth_data, self.range_transform)
 
         return mst_synth_data
+
+class PATECTGAN(Synthesizer):
+    def __init__(self, epsilon: float, domain_path: str, slide_range: int = None):
+        preprocess_factor = 0.1
+        self.synthesizer = PytorchDPSynthesizer(epsilon=epsilon, PATECTGAN(preprocess_epsilon=(preprocess_factor * epsilon)), preprocessor = None)
+        super().__init__(epsilon, domain_path, slide_range)
+        
+    def fit(self, paper: pd.DataFrame):
+        if paper.isnull().any().any():
+            paper = paper.fillna(0)
+            
+        df_patectgan = paper[paper.columns].round(0).astype(int)
+
+        self.synthesizer.fit(df_patectgan, categorical_columns=list(df_patectgan.columns), transformer=BaseTransformer)
+        sample_size = len(df_patectgan)
+        patectgan_synth_data = self.synthesizer.sample(sample_size)
+
+        if self.slide_range:
+            paper, range_transform = self.slide_range_forward(paper)
+            self.range_transform = range_transform
+    
+    def sample(self, n, slide_range=None):
+        patectgan_synth_data = self.synthesizer.sample(n)
+        
+        if slide_range:
+            patectgan_synth_data = self.slide_range_backward(patectgan_synth_data, self.range_transform)
+
+        return patectgan_synth_data
+
+        # patectgan_synth_data.to_pickle(folder_name + 'patectgan_' + str(it) + '.pickle')
+        
+class PrivBayes(Synthesizer):
+    def __init__(self, epsilon: float, domain_path: str, slide_range: int = None) -> None:
+        self.synthesizer = PrivBayes(epsilon=epsilon, domain_path=domain_path)
+        super().__init__(epsilon, domain_path, slide_range)
+        candidate_keys = {'index': True}
+        threshold_value = 40
