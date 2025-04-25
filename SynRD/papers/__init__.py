4750067d12516bbff62e44ce23d06116bb181fc4
Lucas Rosenblatt <60412438+lurosenb@users.noreply.github.com>
2025-04-25 12:35:43 -0400

    hot fix

--- SynRD/papers/__init__.py
+++ SynRD/papers/__init__.py
@@ -2,7 +2,6 @@ from .saw2018cross import Saw2018Cross
 from .fairman2019marijuana import Fairman2019Marijuana
 from .iverson22football import Iverson22Football
 from .jeong2021math import Jeong2021Math
-from .fruiht2018naturally import Fruiht2018Naturally
 from .lee2021ability import Lee2021Ability
 from .pierce2019who import Pierce2019Who
 from .assari2019baseline import Assari2019Baseline
@@ -10,7 +9,6 @@ __all__ = ["Saw2018Cross",
             "Iverson22Football",
             "Fairman2019Marijuana",
             "Jeong2021Math",
-            "Fruiht2018Naturally",
             "Lee2021Ability",
             "Pierce2019Who",
             "Assari2019Baseline"]
